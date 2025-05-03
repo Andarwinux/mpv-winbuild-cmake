b9cf60ff79a4b7bf82b83edc4fe3504ebef2879b
Andarwinux <Andarwinux@users.noreply.github.com>
2025-05-03 00:00:00 +0000

    libdvdread: switch to meson

--- packages/libdvdread.cmake
+++ packages/libdvdread.cmake
@@ -5,12 +5,12 @@ ExternalProject_Add(libdvdread
     GIT_CLONE_FLAGS "--depth=1 --filter=tree:0"
     GIT_PROGRESS TRUE
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${autoreshit}
-    COMMAND ${EXEC} CONF=1 ./configure
-        ${autoshit_confuck_args}
-        --with-libdvdcss
-    BUILD_COMMAND ${MAKE} HIDE=1
-    INSTALL_COMMAND ${MAKE} install
+    CONFIGURE_COMMAND ${EXEC} sed -i [['s/import(.*/false/']] <SOURCE_DIR>/meson.build
+    COMMAND ${EXEC} CONF=1 meson setup --reconfigure <BINARY_DIR> <SOURCE_DIR>
+        ${meson_conf_args}
+        -Dlibdvdcss=enabled
+    BUILD_COMMAND ${EXEC} HIDE=1 ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} meson install -C <BINARY_DIR> --only-changed --tags devel
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 

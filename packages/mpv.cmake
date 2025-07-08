1ea918f49ae07c4f625af0bf369a73bf889624af
Andarwinux <Andarwinux@users.noreply.github.com>
2025-07-08 00:00:00 +0000

    mpv: add legacy build support (without consoleAllocationPolicy)

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -1,5 +1,34 @@
 get_property(src_luajit TARGET luajit PROPERTY _EP_SOURCE_DIR)
 get_property(src_luajit_wrap TARGET luajit-wrap PROPERTY _EP_SOURCE_DIR)
+set(mpv_conf
+        ${meson_conf_args}
+        --force-fallback-for=luajit
+        -Db_lto=true
+        -Db_lto_mode=thin
+        -Dmanpage-build=disabled
+        -Dlua=luajit
+        -Dluajit:amalgam=true
+        -Dluajit:lua52compat=true
+        -Dluajit:sysmalloc=true
+        -Dluajit:luajit=false
+        -Djavascript=enabled
+        -Dsdl2=enabled
+        -Dlibarchive=enabled
+        -Dlibbluray=enabled
+        -Ddvdnav=enabled
+        -Duchardet=enabled
+        -Drubberband=enabled
+        -Dlcms2=enabled
+        -Dopenal=enabled
+        -Dspirv-cross=enabled
+        -Dvulkan=enabled
+        -Dvapoursynth=enabled
+        -Dsixel=enabled
+        -Dwin32-subsystem=console
+        -Dbuild-date=true
+        -Dgl=enabled
+        -Degl-angle=enabled
+)
 ExternalProject_Add(mpv
     DEPENDS
         angle-headers
@@ -35,75 +64,40 @@ ExternalProject_Add(mpv
     COMMAND ${CMAKE_COMMAND} -E copy_directory ${src_luajit} <SOURCE_DIR>/subprojects/luajit
     COMMAND ${CMAKE_COMMAND} -E copy_directory ${src_luajit_wrap}/subprojects/packagefiles/luajit <SOURCE_DIR>/subprojects/luajit
     COMMAND ${EXEC} CONF=1 meson setup --reconfigure <BINARY_DIR> <SOURCE_DIR>
-        ${meson_conf_args}
-        --force-fallback-for=luajit
-        -Db_lto=true
-        -Db_lto_mode=thin
+        ${mpv_conf}
         -Dlibmpv=false
         -Dcplayer=true
-        -Dmanpage-build=disabled
-        -Dlua=luajit
-        -Dluajit:amalgam=true
-        -Dluajit:lua52compat=true
-        -Dluajit:sysmalloc=true
-        -Dluajit:luajit=false
-        -Djavascript=enabled
-        -Dsdl2=enabled
-        -Dlibarchive=enabled
-        -Dlibbluray=enabled
-        -Ddvdnav=enabled
-        -Duchardet=enabled
-        -Drubberband=enabled
-        -Dlcms2=enabled
-        -Dopenal=enabled
-        -Dspirv-cross=enabled
-        -Dvulkan=enabled
-        -Dvapoursynth=enabled
-        -Dsixel=enabled
-        -Dwin32-subsystem=console
-        -Dbuild-date=true
-        -Dgl=enabled
-        -Degl-angle=enabled
     COMMAND ${EXEC} CONF=1 meson setup --reconfigure <BINARY_DIR>/libmpv <SOURCE_DIR>
-        ${meson_conf_args}
-        --force-fallback-for=luajit
+        ${mpv_conf}
         -Ddefault_library=shared
-        -Db_lto=true
-        -Db_lto_mode=thin
+        -Dluajit:default_library=static
         -Dlibmpv=true
         -Dcplayer=false
-        -Dmanpage-build=disabled
-        -Dlua=luajit
-        -Dluajit:amalgam=true
-        -Dluajit:lua52compat=true
-        -Dluajit:sysmalloc=true
-        -Dluajit:luajit=false
-        -Dluajit:default_library=static
-        -Djavascript=enabled
-        -Dsdl2=enabled
-        -Dlibarchive=enabled
-        -Dlibbluray=enabled
-        -Ddvdnav=enabled
-        -Duchardet=enabled
-        -Drubberband=enabled
-        -Dlcms2=enabled
-        -Dopenal=enabled
-        -Dspirv-cross=enabled
-        -Dvulkan=enabled
-        -Dvapoursynth=enabled
-        -Dsixel=enabled
-        -Dwin32-subsystem=console
-        -Dbuild-date=true
-        -Dgl=enabled
-        -Degl-angle=enabled
     BUILD_COMMAND ${EXEC} PDB=1 EXCEP=1 HIDE=1 ninja -C <BINARY_DIR>
           COMMAND ${EXEC} PDB=1 EXCEP=1 ninja -C <BINARY_DIR>/libmpv
     INSTALL_COMMAND ${EXEC} PDB=1 EXCEP=1 meson install -C <BINARY_DIR>/libmpv --only-changed --tags devel
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
-ExternalProject_Add_Step(mpv copy-binary
+if(ENABLE_LEGACY_MPV)
+set(copy-binary-dep build-legacy)
+ExternalProject_Add_Step(mpv build-legacy
     DEPENDEES install
+    LOG 1
+    COMMAND ${EXEC} CONF=1 meson setup --reconfigure <BINARY_DIR>/legacy <SOURCE_DIR>
+        ${mpv_conf}
+        -Dlibmpv=false
+        -Dcplayer=true
+        -Dwin32-subsystem=windows
+    COMMAND ${EXEC} PDB=1 EXCEP=1 HIDE=1 ninja -C <BINARY_DIR>/legacy mpv.exe
+    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/legacy/mpv.exe ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv-legacy.exe
+)
+else()
+set(copy-binary-dep install)
+endif()
+
+ExternalProject_Add_Step(mpv copy-binary
+    DEPENDEES ${copy-binary-dep}
     COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.exe             ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.exe
     COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/mpv.pdb             ${CMAKE_CURRENT_BINARY_DIR}/mpv-debug/mpv.pdb
     COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/libmpv/libmpv-2.pdb ${CMAKE_CURRENT_BINARY_DIR}/mpv-debug/libmpv-2.pdb

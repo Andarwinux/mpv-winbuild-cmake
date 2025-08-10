bbdaeaae34b63a621af80eb19c984b5b0049dfd7
Andarwinux <Andarwinux@users.noreply.github.com>
2025-08-10 00:00:00 +0000

    libunibreak: switch to meson

--- packages/libunibreak.cmake
+++ packages/libunibreak.cmake
@@ -6,14 +6,15 @@ ExternalProject_Add(libunibreak
     UPDATE_COMMAND ""
     CONFIGURE_ENVIRONMENT_MODIFICATION
         _IS_CONFIGURE=set:1
-    CONFIGURE_COMMAND ${autoreshit}
-    COMMAND ${EXEC} ./configure
-        ${autoshit_confuck_args}
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy_directory ${src_meson_wrap}/subprojects/packagefiles/libunibreak <SOURCE_DIR>
+    COMMAND ${EXEC} meson setup --reconfigure <BINARY_DIR> <SOURCE_DIR>
+        ${meson_conf_args}
+        -Dtests=disabled
     BUILD_ENVIRONMENT_MODIFICATION
         _PACKAGE_NAME=set:${package}
         _BINARY_DIR=set:<BINARY_DIR>
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+    BUILD_COMMAND ${EXEC} meson install -C <BINARY_DIR> --only-changed --tags devel
+    INSTALL_COMMAND ""
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 

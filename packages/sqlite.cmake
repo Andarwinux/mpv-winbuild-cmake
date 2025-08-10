24566dcdac1c77ad7b02dc7bcb757ee4e3ca35e4
Andarwinux <Andarwinux@users.noreply.github.com>
2025-08-10 00:00:00 +0000

    sqlite: switch to meson

--- packages/sqlite.cmake
+++ packages/sqlite.cmake
@@ -1,18 +1,18 @@
 ExternalProject_Add(sqlite
     DEPENDS
-        zlib
-    URL https://www.sqlite.org/2025/sqlite-autoconf-3500300.tar.gz
-    URL_HASH SHA3_256=c3df1542703a666d3f41bb623e9bed7d6e1dc81c57f0c45e3122403f862c520d
+        meson-wrap
+    URL https://www.sqlite.org/2025/sqlite-amalgamation-3500400.zip
+    URL_HASH SHA3_256=f131b68e6ba5fb891cc13ebb5ff9555054c77294cb92d8d1268bad5dba4fa2a1
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     CONFIGURE_ENVIRONMENT_MODIFICATION
         _IS_CONFIGURE=set:1
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR> <BINARY_DIR>
-    COMMAND ${EXEC} ./configure
-        ${autoshit_confuck_args}
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy_directory ${src_meson_wrap}/subprojects/packagefiles/sqlite3 <SOURCE_DIR>
+    COMMAND ${EXEC} meson setup --reconfigure <BINARY_DIR> <SOURCE_DIR>
+        ${meson_conf_args}
     BUILD_ENVIRONMENT_MODIFICATION
         _PACKAGE_NAME=set:${package}
         _BINARY_DIR=set:<BINARY_DIR>
-    BUILD_COMMAND ${MAKE} install-lib install-headers install-pc
+    BUILD_COMMAND ${EXEC} meson install -C <BINARY_DIR> --only-changed --tags devel
     INSTALL_COMMAND ""
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )

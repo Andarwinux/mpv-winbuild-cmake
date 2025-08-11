467df8e4cb9144a7d1c3cfcd7307713ec52992c2
Andarwinux <Andarwinux@users.noreply.github.com>
2025-08-11 00:00:00 +0000

    llvm-compiler: add missed / to -ffile-prefix-map=@PKG_TO_SRC@

--- utils/setup_aarch64.sh
+++ utils/setup_aarch64.sh
@@ -2,12 +2,13 @@
 cmake -DTARGET_ARCH=aarch64-w64-mingw32 \
 -DMARCH=cortex-x3 \
 -DMARCH_NAME='' \
--DSINGLE_SOURCE_LOCATION="/build/src_packages" \
+-DSINGLE_SOURCE_LOCATION="/build/src_packages/" \
 -DCMAKE_INSTALL_PREFIX="/build/install" \
 -DMINGW_INSTALL_PREFIX="/build_aarch64/install/aarch64-w64-mingw32" \
 -DCOMPILER_TOOLCHAIN=clang \
 -DLLVM_ENABLE_LTO=Thin \
 -DENABLE_CCACHE=ON \
+-DCCACHE_MAXSIZE=8G \
 -DCLANG_PACKAGES_PGO=OFF \
 -DCLANG_PACKAGES_PROFDATA_FILE="/build/pgo.profdata" \
 -DCUSTOM_LIBCXX=ON \

11ad344f9c3b752d3837b49db2227e097b14fee0
Andarwinux <Andarwinux@users.noreply.github.com>
2025-08-22 00:00:00 +0000

    toolchain: add -fseparate-named-sections to host cflags

--- packages/qt6-qttools.cmake
+++ packages/qt6-qttools.cmake
@@ -82,8 +82,8 @@ ExternalProject_Add(qt6-qttools
         -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ONLY
         "-DCMAKE_REQUIRED_FLAGS='-O0'"
         "-DCMAKE_REQUIRED_LINK_OPTIONS='-Wl,-O0,--icf=none,--no-gc-sections'"
-        "-DCMAKE_C_FLAGS='-Wno-unused-command-line-argument -w -g0 -fno-ident -fno-temp-file -fno-plt -Wa,--crel,--allow-experimental-crel -march=native -O3 -fno-semantic-interposition -fvisibility=hidden -fno-math-errno -fno-signed-zeros -fno-trapping-math -falign-functions=32 -ffp-contract=fast -ftls-model=local-exec -Xclang -fno-pch-timestamp'"
-        "-DCMAKE_CXX_FLAGS='-Wno-unused-command-line-argument -w -g0 -fno-ident -fno-temp-file -fno-plt -Wa,--crel,--allow-experimental-crel -march=native -O3 -fno-semantic-interposition -fvisibility=hidden -fno-math-errno -fno-signed-zeros -fno-trapping-math -falign-functions=32 -ffp-contract=fast -ftls-model=local-exec -Xclang -fno-pch-timestamp'"
+        "-DCMAKE_C_FLAGS='-Wno-unused-command-line-argument -w -g0 -fno-ident -fno-temp-file -fno-plt -Wa,--crel,--allow-experimental-crel -march=native -O3 -fdata-sections -ffunction-sections -fseparate-named-sections -fno-unique-section-names -fno-semantic-interposition -fvisibility=hidden -fno-math-errno -fno-signed-zeros -fno-trapping-math -falign-functions=32 -ffp-contract=fast -ftls-model=local-exec -Xclang -fno-pch-timestamp'"
+        "-DCMAKE_CXX_FLAGS='-Wno-unused-command-line-argument -w -g0 -fno-ident -fno-temp-file -fno-plt -Wa,--crel,--allow-experimental-crel -march=native -O3 -fdata-sections -ffunction-sections -fseparate-named-sections -fno-unique-section-names -fno-semantic-interposition -fvisibility=hidden -fno-math-errno -fno-signed-zeros -fno-trapping-math -falign-functions=32 -ffp-contract=fast -ftls-model=local-exec -Xclang -fno-pch-timestamp'"
         "-DCMAKE_EXE_LINKER_FLAGS='-fuse-ld=lld -Wl,-Bsymbolic,--build-id=none,-s,-O3,--icf=all,--gc-sections,-zpack-relative-relocs,-zcommon-page-size=2097152,-zmax-page-size=2097152,-zseparate-loadable-segments'"
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install

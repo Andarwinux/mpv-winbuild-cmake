5c2f3bd920e38c895279a483f8e5a1f43e00bb34
Andarwinux <Andarwinux@users.noreply.github.com>
2025-09-15 00:00:00 +0000

    toolchain: enable x86-use-fsrm-for-memcpy for cpus that have avx512

--- utils/llvm-pgo-use.sh
+++ utils/llvm-pgo-use.sh
@@ -8,7 +8,7 @@ cmake -DTARGET_ARCH=x86_64-w64-mingw32 \
 -DLLVM_ENABLE_PGO=USE \
 -DLLVM_PROFDATA_FILE="/build/llvm.profdata" \
 -DCLANG_FLAGS="-mprefer-vector-width=512 -mno-gather" \
--DTOOLCHAIN_FLAGS="-mprefer-vector-width=256 -mno-gather" \
+-DTOOLCHAIN_FLAGS="-mprefer-vector-width=256 -mno-gather -Wl,-mllvm,-x86-use-fsrm-for-memcpy" \
 -DCUSTOM_LIBCXX=ON \
 -DCUSTOM_COMPILER_RT=ON \
 -G Ninja \

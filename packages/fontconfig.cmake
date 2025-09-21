37fa10418cc8961909c4ec8c49a37a3efbe09b2a
Andarwinux <Andarwinux@users.noreply.github.com>
2025-09-21 00:00:00 +0000

    fontconfig: remove dllexport suppression
    upstream fixed

--- packages/fontconfig.cmake
+++ packages/fontconfig.cmake
@@ -28,7 +28,6 @@ ExternalProject_Add(fontconfig
     BUILD_ENVIRONMENT_MODIFICATION
         _PACKAGE_NAME=set:${package}
         _BINARY_DIR=set:<BINARY_DIR>
-        _FORCE_HIDE_DLLEXPORT=set:1
     BUILD_COMMAND ${EXEC} meson install -C <BINARY_DIR> --only-changed --tags devel
     INSTALL_COMMAND ""
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1

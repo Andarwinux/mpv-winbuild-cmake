1556663d87bf08e13a8663dff0bb87f2fa8527e1
Andarwinux <Andarwinux@users.noreply.github.com>
2025-10-28 00:00:00 +0000

    toolchain: remove unused stuff

--- toolchain/mingw-w64/mingw-w64-headers.cmake
+++ toolchain/mingw-w64/mingw-w64-headers.cmake
@@ -18,8 +18,6 @@ ExternalProject_Add(mingw-w64-headers
             COMMAND ${CMAKE_COMMAND} -E copy ${MINGW_INSTALL_PREFIX}/include/shlobj.h ${MINGW_INSTALL_PREFIX}/include/Shlobj.h
             COMMAND ${CMAKE_COMMAND} -E copy ${MINGW_INSTALL_PREFIX}/include/shellapi.h ${MINGW_INSTALL_PREFIX}/include/Shellapi.h
             COMMAND ${CMAKE_COMMAND} -E copy ${MINGW_INSTALL_PREFIX}/include/objbase.h ${MINGW_INSTALL_PREFIX}/include/Objbase.h
-            COMMAND ${CMAKE_COMMAND} -E make_directory ${MINGW_INSTALL_PREFIX}/lib
-            COMMAND ${EXEC} ${TARGET_ARCH}-dlltool -d ${CMAKE_CURRENT_SOURCE_DIR}/mingw-w64/mingw-w64-math.def -l ${MINGW_INSTALL_PREFIX}/lib/ucrtmath.a
             COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/headers/icu.h ${MINGW_INSTALL_PREFIX}/include/
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )

c3b7d32dae4302d706f7ef998af1b3900f731cc9
Andarwinux <Andarwinux@users.noreply.github.com>
2025-11-16 00:00:00 +0000

    libjxl: force strip -fno-builtin-*

--- packages/libjxl.cmake
+++ packages/libjxl.cmake
@@ -50,7 +50,7 @@ ExternalProject_Add(libjxl
         _PACKAGE_NAME=set:${package}
         _BINARY_DIR=set:<BINARY_DIR>
         _FORCE_HIDE_DLLEXPORT=set:1
-    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR> libjxl.a libjxl_cms.a libjxl_threads.a
+    BUILD_COMMAND ${EXEC} "CCC_OVERRIDE_OPTIONS='x-fno-builtin-fwrite x-fno-builtin-fread'" ninja -C <BINARY_DIR> libjxl.a libjxl_cms.a libjxl_threads.a
     INSTALL_COMMAND ${EXEC} ${CMAKE_COMMAND} --install <BINARY_DIR>
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )

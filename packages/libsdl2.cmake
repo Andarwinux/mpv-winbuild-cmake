6ab781d80c4a62ae1601c710437c9f3356514886
Andarwinux <Andarwinux@users.noreply.github.com>
2026-01-17 00:00:00 +0000

    libsdl2: disable unused features

--- packages/libsdl2.cmake
+++ packages/libsdl2.cmake
@@ -1,8 +1,6 @@
 ExternalProject_Add(libsdl2
     DEPENDS
-        vulkan
         libiconv
-        libsamplerate
     GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--depth=1 --no-single-branch --sparse --filter=tree:0"
@@ -16,11 +14,13 @@ ExternalProject_Add(libsdl2
     CONFIGURE_COMMAND ${EXEC} ${CMAKE_COMMAND} -H<SOURCE_DIR> -B<BINARY_DIR>
         ${cmake_conf_args}
         ${libsdl2_force_skip_check}
-        -DSDL_VULKAN=ON
+        -DSDL_AUDIO=OFF
+        -DSDL_VIDEO=OFF
+        -DSDL_RENDER=OFF
+        -DSDL_CPUINFO=OFF
+        -DSDL_VULKAN=OFF
         -DSDL_LIBICONV=ON
         -DSDL_SYSTEM_ICONV=ON
-        -DSDL_LIBSAMPLERATE=ON
-        -DSDL_LIBSAMPLERATE_SHARED=OFF
         -DSDL_MMX=OFF
         -DSDL_SSE=OFF
         -DSDL_SSE2=OFF
@@ -35,14 +35,13 @@ ExternalProject_Add(libsdl2
         -DSDL_OPENGLES=OFF
         -DHAVE_GDWARF_4=OFF
         -DHAVE_GCC_NO_STRICT_ALIASING=OFF
-        "-DCMAKE_C_FLAGS='-U__MMX__ -DSDL_CPUINFO_DISABLED'"
-        "-DCMAKE_CXX_FLAGS='-U__MMX__ -DSDL_CPUINFO_DISABLED'"
+        "-DCMAKE_C_FLAGS='-U__MMX__ -DDYNAPI_NEEDS_DLOPEN'"
+        "-DCMAKE_CXX_FLAGS='-U__MMX__ -DDYNAPI_NEEDS_DLOPEN'"
     BUILD_ENVIRONMENT_MODIFICATION
         _PACKAGE_NAME=set:${package}
         _BINARY_DIR=set:<BINARY_DIR>
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ${CMAKE_COMMAND} --install <BINARY_DIR>
-    COMMAND ${EXEC} sed -i [['s/-lSDL2/-lSDL2 -lsamplerate/']] ${MINGW_INSTALL_PREFIX}/lib/pkgconfig/sdl2.pc
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 

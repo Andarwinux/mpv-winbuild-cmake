bcb0757d9f72a3462d67e3ab7cf1a81886d7ee9c
Andarwinux <Andarwinux@users.noreply.github.com>
2026-02-14 00:00:00 +0000

    llvm-libc: build strlen with builtin
    latest find_first_character change in llvm-libc seems fixed UB with LTO inline

--- utils/pgo-gen.sh
+++ utils/pgo-gen.sh
@@ -2,12 +2,12 @@
 cmake -DTARGET_ARCH=x86_64-w64-mingw32 \
 -DMARCH=native \
 -DCOMPILER_TOOLCHAIN=clang \
--DLLVM_ENABLE_LTO=OFF \
+-DLLVM_ENABLE_LTO=Full \
 -DLLVM_ENABLE_UNSAFE_X86_AVX512_VFABI=ON \
 -DENABLE_CCACHE=OFF \
 -DCLANG_PACKAGES_PGO=GEN \
 -DCLANG_FLAGS="" \
--DTOOLCHAIN_FLAGS="-mprefer-vector-width=512 -mno-gather -Wl,-mllvm,-x86-use-fsrm-for-memcpy" \
+-DTOOLCHAIN_FLAGS="-mprefer-vector-width=256 -mno-gather -Wl,-mllvm,-x86-use-fsrm-for-memcpy,-mllvm,-lv-strided-pointer-ivs" \
 -DCUSTOM_LIBCXX=ON \
 -DCUSTOM_COMPILER_RT=ON \
 -G Ninja \

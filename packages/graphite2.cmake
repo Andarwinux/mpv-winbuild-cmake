70778983116af4b2d90db66208c45a84b57b3a21
Andarwinux <Andarwinux@users.noreply.github.com>
2026-02-16 00:00:00 +0000

    harfbuzz: enable graphite2

---
+++ packages/graphite2.cmake
@@ -0,0 +1,25 @@
+ExternalProject_Add(graphite2
+    GIT_REPOSITORY https://github.com/silnrsi/graphite.git
+    SOURCE_DIR ${SOURCE_LOCATION}
+    GIT_CLONE_FLAGS "--depth=1 --sparse --filter=tree:0"
+    GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests"
+    GIT_PROGRESS TRUE
+    UPDATE_COMMAND ""
+    CONFIGURE_ENVIRONMENT_MODIFICATION
+        _IS_CONFIGURE=set:1
+    CONFIGURE_COMMAND ${EXEC} ${CMAKE_COMMAND} -H<SOURCE_DIR> -B<BINARY_DIR>
+        ${cmake_conf_args}
+        -DGRAPHITE2_NFILEFACE=ON
+        -DGRAPHITE2_NTRACING=OFF
+        -DGRAPHITE2_TELEMETRY=OFF
+    BUILD_ENVIRONMENT_MODIFICATION
+        _PACKAGE_NAME=set:${package}
+        _BINARY_DIR=set:<BINARY_DIR>
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ${CMAKE_COMMAND} --install <BINARY_DIR>
+            COMMAND ${EXEC} sed -i [['s/GRAPHITE2_STATIC/_WIN32/g']] ${MINGW_INSTALL_PREFIX}/include/graphite2/Types.h
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_PATCH 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+force_rebuild_git(graphite2)
+cleanup(graphite2 install)

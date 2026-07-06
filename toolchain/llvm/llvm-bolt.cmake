5f14a7d656c5e74795fab90f02ff1b093f5ead99
Andarwinux <Andarwinux@users.noreply.github.com>
2026-07-06 00:00:00 +0000

    Revert "llvm-bolt: disable for now"
    This reverts commit 876665804da9ba051a8aa92a1eeb23c084dc9734.

--- toolchain/llvm/llvm-bolt.cmake
+++ toolchain/llvm/llvm-bolt.cmake
@@ -7,67 +7,67 @@ ExternalProject_Add(llvm-bolt
     DOWNLOAD_DIR ${SOURCE_LOCATION}
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND "" #${EXEC} llvm-bolt
-        #${CMAKE_INSTALL_PREFIX}/llvmbin/llvm
-        #-o ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr
-        #--instrument
-        #--instrumentation-file-append-pid
-        #--instrumentation-file=${CMAKE_INSTALL_PREFIX}/llvm-bolt/llvm
-        #--lite=false
-    #COMMAND ${EXEC} ln -s ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr ld.lld 2>/dev/null || true
-    #COMMAND ${EXEC} mkdir -p ${CMAKE_INSTALL_PREFIX}/llvm-bolt
-    #COMMAND ${EXEC} ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr clang   ${bolt_training_common_args} ${llvm_mllvm_lto} -mllvm -unroll-full-max-count=0 -flto=thin -fwhole-program-vtables sqlite3.c shell.c -o sqlite3_lto.exe &
-    #        ${EXEC} ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr clang   ${bolt_training_common_args} ${MLLVM_FLAGS} -fno-lto -fno-whole-program-vtables sqlite3.c shell.c -o sqlite3.exe &
-    #        ${EXEC} ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr clang++ ${bolt_training_common_args} ${llvm_mllvm_lto} -mllvm -unroll-full-max-count=0 -flto=thin -fwhole-program-vtables ${SINGLE_SOURCE_LOCATION}/harfbuzz/src/harfbuzz.cc -shared "-DHB_EXTERN='[[gnu::dllexport]]'" -o harfbuzz_lto.dll &
-    #        ${EXEC} ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr clang++ ${bolt_training_common_args} ${MLLVM_FLAGS} -fno-lto -fno-whole-program-vtables ${SINGLE_SOURCE_LOCATION}/harfbuzz/src/harfbuzz.cc -shared "-DHB_EXTERN='[[gnu::dllexport]]'" -o harfbuzz.dll
-    #COMMAND ${EXEC} rm sqlite3.exe sqlite3_lto.exe harfbuzz.dll harfbuzz_lto.dll ld.lld ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr
-    #COMMAND ${EXEC} merge-fdata ${CMAKE_INSTALL_PREFIX}/llvm-bolt/* -o llvm.fdata
-    #COMMAND ${EXEC} rm -r ${CMAKE_INSTALL_PREFIX}/llvm-bolt
-    #COMMAND ${EXEC} llvm-bolt
-        #--data llvm.fdata
-        #${CMAKE_INSTALL_PREFIX}/llvmbin/llvm
-        #-o ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.bolt
-        #--align-blocks
-        #--bolt-info=false
-        #--cg-use-split-hot-size
-        #--cmov-conversion
-        #--dyno-stats
-        #--elim-link-veneers
-        #--eliminate-unreachable
-        #--fix-block-counts
-        #--fix-func-counts
-        #--frame-opt-rm-stores
-        #--frame-opt=all
-        #--group-stubs
-        #--hot-data
-        #--hot-text
-        #--icf=all
-        #--icp-inline
-        #--icp-jump-tables-targets
-        #--icp=jump-tables
-        #--infer-fall-throughs
-        #--inline-ap
-        #--inline-small-functions
-        #--iterative-guess
-        #--jump-tables=aggressive
-        #--min-branch-clusters
-        #--peepholes=all
-        #--plt=all
-        #--reorder-blocks=ext-tsp
-        #--reorder-functions=cdsort
-        #--sctc-mode=always
-        #--simplify-conditional-tail-calls
-        #--simplify-rodata-loads
-        #--split-all-cold
-        #--split-eh
-        #--split-functions
-        #--split-strategy=cdsplit
-        #--stoke
-        #--tail-duplication=cache
-        #--three-way-branch
-    #COMMAND ${EXEC} llvm-strip -s ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.bolt
-    #COMMAND ${EXEC} rm -r <SOURCE_DIR>/llvm.fdata
-    INSTALL_COMMAND "" #${EXEC} mv ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.bolt ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm
+    BUILD_COMMAND ${EXEC} llvm-bolt
+        ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm
+        -o ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr
+        --instrument
+        --instrumentation-file-append-pid
+        --instrumentation-file=${CMAKE_INSTALL_PREFIX}/llvm-bolt/llvm
+        --lite=false
+    COMMAND ${EXEC} ln -s ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr ld.lld 2>/dev/null || true
+    COMMAND ${EXEC} mkdir -p ${CMAKE_INSTALL_PREFIX}/llvm-bolt
+    COMMAND ${EXEC} ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr clang   ${bolt_training_common_args} ${llvm_mllvm_lto} -mllvm -unroll-full-max-count=0 -flto=thin -fwhole-program-vtables sqlite3.c shell.c -o sqlite3_lto.exe &
+            ${EXEC} ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr clang   ${bolt_training_common_args} ${MLLVM_FLAGS} -fno-lto -fno-whole-program-vtables sqlite3.c shell.c -o sqlite3.exe &
+            ${EXEC} ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr clang++ ${bolt_training_common_args} ${llvm_mllvm_lto} -mllvm -unroll-full-max-count=0 -flto=thin -fwhole-program-vtables ${SINGLE_SOURCE_LOCATION}/harfbuzz/src/harfbuzz.cc -shared "-DHB_EXTERN='[[gnu::dllexport]]'" -o harfbuzz_lto.dll &
+            ${EXEC} ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr clang++ ${bolt_training_common_args} ${MLLVM_FLAGS} -fno-lto -fno-whole-program-vtables ${SINGLE_SOURCE_LOCATION}/harfbuzz/src/harfbuzz.cc -shared "-DHB_EXTERN='[[gnu::dllexport]]'" -o harfbuzz.dll
+    COMMAND ${EXEC} rm sqlite3.exe sqlite3_lto.exe harfbuzz.dll harfbuzz_lto.dll ld.lld ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.instr
+    COMMAND ${EXEC} merge-fdata ${CMAKE_INSTALL_PREFIX}/llvm-bolt/* -o llvm.fdata
+    COMMAND ${EXEC} rm -r ${CMAKE_INSTALL_PREFIX}/llvm-bolt
+    COMMAND ${EXEC} llvm-bolt
+        --data llvm.fdata
+        ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm
+        -o ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.bolt
+        --align-blocks
+        --bolt-info=false
+        --cg-use-split-hot-size
+        --cmov-conversion
+        --dyno-stats
+        --elim-link-veneers
+        --eliminate-unreachable
+        --fix-block-counts
+        --fix-func-counts
+        --frame-opt-rm-stores
+        --frame-opt=all
+        --group-stubs
+        --hot-data
+        --hot-text
+        --icf=all
+        --icp-inline
+        --icp-jump-tables-targets
+        --icp=jump-tables
+        --infer-fall-throughs
+        --inline-ap
+        --inline-small-functions
+        --iterative-guess
+        --jump-tables=aggressive
+        --min-branch-clusters
+        --peepholes=all
+        --plt=all
+        --reorder-blocks=ext-tsp
+        --reorder-functions=cdsort
+        --sctc-mode=always
+        --simplify-conditional-tail-calls
+        --simplify-rodata-loads
+        --split-all-cold
+        --split-eh
+        --split-functions
+        --split-strategy=cdsplit
+        --stoke
+        --tail-duplication=cache
+        --three-way-branch
+    COMMAND ${EXEC} llvm-strip -s ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.bolt
+    COMMAND ${EXEC} rm -r <SOURCE_DIR>/llvm.fdata
+    INSTALL_COMMAND ${EXEC} mv ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm.bolt ${CMAKE_INSTALL_PREFIX}/llvmbin/llvm
     BUILD_IN_SOURCE 1
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
